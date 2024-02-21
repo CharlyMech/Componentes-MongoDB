4b03d9109bc447280944583b3bf18f078351af1a
CharlyMech <sanchezreciocarlos99@gmail.cim>
2024-02-21 21:20:34 +0100

    ADD Department class filled with setters, getters and toString

--- src/main/java/cesur/accesodatos/mongodb/Department.java
+++ src/main/java/cesur/accesodatos/mongodb/Department.java
@@ -1,6 +1,60 @@
 package cesur.accesodatos.mongodb;
 
+import org.bson.Document;
+
 public class Department {
 	// Class variables
 	private Integer depno;
+	private String name;
+	private String location;
+
+	// Constructors
+	public Department(Integer depno, String name, String location) {
+		this.depno = depno;
+		this.name = name;
+		this.location = location;
+	}
+
+	public Department() { // Just in case
+	}
+
+	// Class methods
+	public Department fromDocumentToDepartment(Document deptDoc) {
+		return null;
+	}
+
+	// GETTERS //
+	public int getDepno() {
+		return depno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	// SETTERS //
+	public void setDepno(int depno) {
+		this.depno = depno;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	// TO STRING //
+	@Override
+	public String toString() {
+		return "Department{" +
+				"depno=" + depno +
+				", name='" + name + '\'' +
+				", location='" + location + '\'' +
+				'}';
+	}
 }
